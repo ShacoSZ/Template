cfcbc6fc2299b5f51bf276cd0928b74e9cd51947
Hector Flores <hectorflores_rdz@outlook.com>
2023-02-18 14:38:27 -0600

    nav

--- TiendaMusica/src/app/app-routing.module.ts
+++ TiendaMusica/src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:"",redirectTo:"Home", pathMatch:"full"},
+  {path:"Home",component:HomeComponent},
+  {path:"**", component:PageNotFoundComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
