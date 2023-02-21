412f487100379b5d01c40c96857088e6b1de3456
Hector Flores <hectorflores_rdz@outlook.com>
2023-02-21 02:18:02 -0600

    titulos

--- TiendaMusica/src/app/Services/modal.service.ts
+++ TiendaMusica/src/app/Services/modal.service.ts
@@ -7,35 +7,5 @@ import { ModalesComponent as ModalComponent } from '../components/modales/modale
 export class ModalService {
   private modals: ModalComponent[] = [];
 
-  add(modal: ModalComponent) {
-      // ensure component has a unique id attribute
-      if (!modal.id || this.modals.find(x => x.id === modal.id)) {
-          throw new Error('modal must have a unique id attribute');
-      }
-
-      // add modal to array of active modals
-      this.modals.push(modal);
-  }
-
-  remove(modal: ModalComponent) {
-      // remove modal from array of active modals
-      this.modals = this.modals.filter(x => x === modal);
-  }
-
-  open(id: string) {
-      // open modal specified by id
-      const modal = this.modals.find(x => x.id === id);
-
-      if (!modal) {
-          throw new Error(`modal '${id}' not found`);
-      }
-
-      modal.open();
-  }
-
-  close() {
-      // close the modal that is currently open
-      const modal = this.modals.find(x => x.isOpen);
-      modal?.close();
-  }
+  
 }
