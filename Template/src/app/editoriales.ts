12ced6f17e16f135a77f123c760cbddefb97f473
salazar <shadowsalazar@hotmail.com>
2023-03-27 23:10:42 -0600

    i deleted some little things

--- Template/src/app/editoriales.ts
+++
@@ -1,7 +0,0 @@
-export class editorial{
-    id?:number;
-    nombre?:string;
-    correo?:string;
-    direccion?:string;
-    telefono?:number;
-}
--- Template/src/app/librosIdiomas.ts
+++
@@ -1,7 +0,0 @@
-export class libroIdioma{
-    id?:number;
-    libro_id?:number;
-    idioma_id?:number;
-    nombre?:string;
-    idioma?:string;
-}
